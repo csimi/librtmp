2e346c868013ace973139047525aac77b16604be
David Csirmaz <csimika@gmail.com>
2019-09-10 09:24:06 +0200

    handle socket errors

--- src/rtmp.js
+++ src/rtmp.js
@@ -5,6 +5,7 @@ const StreamFactory = require('./stream_factory')
 const connect = async ({host = 'localhost', port = 1935, app, swfUrl, tcUrl, pageUrl} = {}, Handshake = PlainHandshake) =>
   new Promise((resolve, reject) => {
     const socket = net.connect({host, port})
+    socket.once('error', reject)
     const handshake = new Handshake(socket)
     handshake.once("uninitialized", () => handshake.sendC0C1())
     handshake.once("handshake:done", async () => {
